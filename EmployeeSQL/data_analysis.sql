49e71a6462cfdfd5e368e753a3747a64d4742f0d
VinnyShankar <vinayakshank@gmail.com>
2023-04-28 20:37:18 -0700

    data analysis

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -62,3 +62,16 @@ ON e.emp_no = de.emp_no
 INNER JOIN departments d
 ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales';
+
+-- List each employee in the Sales and Development departments, including their 
+-- employee number, last name, first name, and department name (4 points)
+SELECT e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+FROM employees e
+INNER JOIN dept_emp de
+ON e.emp_no = de.emp_no
+INNER JOIN departments d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales','Development');
