b7c95fe3dd6c91fd156d55abecd90996711b13d5
VinnyShankar <vinayakshank@gmail.com>
2023-04-28 20:33:23 -0700

    data analysis

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -50,3 +50,15 @@ SELECT first_name,
 FROM employees
 WHERE last_name LIKE 'B%'
 AND first_name = 'Hercules';
+
+-- List each employee in the Sales department, including their 
+-- employee number, last name, and first name (2 points)
+SELECT e.emp_no,
+	e.last_name,
+	e.first_name
+FROM employees e
+INNER JOIN dept_emp de
+ON e.emp_no = de.emp_no
+INNER JOIN departments d
+ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
